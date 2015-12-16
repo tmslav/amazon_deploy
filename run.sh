e0303336a7aa34cdd5b0572ee00c87ae1803b8c5
Tomislav R <tmslav@gmail.com>
2015-12-16 23:01:59 +0100

    2

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /home/ubuntu/
-rm -r amazon_deploy
+yes | rm -rf amazon_deploy
 git pull https://github.com/tmslav/amazon_deploy.git
 tmux new-session -d "/usr/bin/python  /home/ubuntu/amazon_deploy/app.py"
