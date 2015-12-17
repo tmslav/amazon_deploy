ff21cab8e997ea941e8b51f70bfcf3c42a3f9802
Tomislav R <tmslav@gmail.com>
2015-12-17 03:40:42 +0100

    tst

--- app.py
+++ app.py
@@ -13,31 +13,22 @@ def r():
 def state():
     return br.state,200
 
-@app.route("/send_code",methods=['POST'])
+@app.route("/action",methods=['POST'])
 def request_accept():
     try:
         pd = ujson.loads(request.get_data())
-        print pd
-        ret = br.enter_code(pd['code'])
-        return ujson.dumps(ret),200
+        if pd.get("code"):
+            ret = br.enter_code(pd['code'])
+            return ujson.dumps(ret),200
+        else:
+            br.navigate_to_login()
+            br.login(pd['username'],pd['password'])
+            br.navigate_to_code_reedem()
+            return 'OK',200
     except:
         import traceback
         traceback.print_exc()
         return "ERROR",400
-
-@app.route("/login",methods=['POST'])
-def login_to_amazon():
-    try:
-        pd=ujson.loads(request.get_data())
-        br.navigate_to_login()
-        br.login(pd['username'],pd['password'])
-        br.navigate_to_code_reedem()
-        return 'OK',200
-    except:
-        return "ERROR:{}".format(request.get_data()),400
-
-
-
 
 if __name__=='__main__':
     app.run(port=5000,host="0.0.0.0")
